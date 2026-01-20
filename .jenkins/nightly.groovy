d61b0a5978b66663ea940c7014abe823733ba180
Daniel Arndt <arndtd@ornl.gov>
2026-01-20 11:02:24 -0500

    Nightly CI spack: Verbose install kokkos (#8828)
    * Nightly CI spack: Verbose install kokkos
    
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>
    
    * Add -v to more lines
    
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>
    
    ---------
    
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- .jenkins/nightly.groovy
+++ .jenkins/nightly.groovy
@@ -35,8 +35,8 @@ pipeline {
                           rm -rf spack && \
                           git clone https://github.com/spack/spack.git && \
                           . ./spack/share/spack/setup-env.sh && \
-                          spack install --only=dependencies kokkos@develop+tests && \
-                          spack install --only=package ${CDASH_ARGS} kokkos@develop+tests cxxstd=20 && \
+                          spack install -v --only=dependencies kokkos@develop+tests && \
+                          spack install -v --only=package ${CDASH_ARGS} kokkos@develop+tests cxxstd=20 && \
                           spack load cmake && \
                           spack test run ${CDASH_ARGS} kokkos && \
                           spack test results -l
@@ -67,8 +67,8 @@ pipeline {
                           rm -rf spack && \
                           git clone https://github.com/spack/spack.git && \
                           . ./spack/share/spack/setup-env.sh && \
-                          spack install --only=dependencies kokkos@develop+cuda+wrapper+tests cuda_arch=80 ^cuda@12.9.0 && \
-                          spack install --only=package ${CDASH_ARGS} kokkos@develop+cuda+wrapper+tests cuda_arch=80 ^cuda@12.9.0 && \
+                          spack install -v --only=dependencies kokkos@develop+cuda+wrapper+tests cuda_arch=80 ^cuda@12.9.0 && \
+                          spack install -v --only=package ${CDASH_ARGS} kokkos@develop+cuda+wrapper+tests cuda_arch=80 ^cuda@12.9.0 && \
                           spack load cmake  && \
                           spack load kokkos-nvcc-wrapper && \
                           spack load cuda && \
