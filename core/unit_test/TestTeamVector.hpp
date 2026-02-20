629f4b68826213d9fb20c224775fec00470382ca
Damien L-G <dalg24@gmail.com>
2026-02-20 15:16:16 -0500

    Guard tests that had non-power of 2 team policy vector lengths
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/TestTeamVector.hpp
+++ core/unit_test/TestTeamVector.hpp
@@ -1046,10 +1046,8 @@ TEST(TEST_CATEGORY, triple_nested_parallelism) {
   {
 #ifdef KOKKOS_ENABLE_DEPRECATED_CODE_5
     TestTripleNestedReduce<double, TEST_EXECSPACE>(8192, 2048, 16, 33);
-#else
-    TestTripleNestedReduce<double, TEST_EXECSPACE>(8192, 2048, 16, 31);
-#endif
     TestTripleNestedReduce<double, TEST_EXECSPACE>(8192, 2048, 16, 19);
+#endif
   }
   TestTripleNestedReduce<double, TEST_EXECSPACE>(8192, 2048, 16, 16);
   TestTripleNestedReduce<double, TEST_EXECSPACE>(8192, 2048, 7, 16);
