030a1f6d13a6ba8242fb7737f94272bfe7525a83
Nemanja Grujic <nemanja.grujic@nextsilicon.com>
2026-03-30 16:47:25 -0500

    nextsilicon: Use `__builtin_trap()` for `nextsilicon_abort()`
    Co-authored-by: Patrick Fasano <patrick.fasano@nextsilicon.com>
    Signed-off-by: Patrick Fasano <patrick.fasano@nextsilicon.com>

--- core/src/NextSilicon/Kokkos_NextSilicon_Abort.hpp
+++ core/src/NextSilicon/Kokkos_NextSilicon_Abort.hpp
@@ -4,13 +4,21 @@
 #ifndef KOKKOS_NEXTSILICON_ABORT_HPP
 #define KOKKOS_NEXTSILICON_ABORT_HPP
 
+#include <NextSilicon/Kokkos_NextSilicon_Abort.hpp>
+
+#include <nextapi/intrinsics.h>
+#include <cstdio>
+
 namespace Kokkos {
 namespace Impl {
 
 [[noreturn]] inline void nextsilicon_abort(char const* msg) {
-  // FIXME_NEXTSILICON Add nextapi to abort application from device
-  while (true)
-    ;
+  // FIXME_NEXTSILICON: We need to add printing from handed off code also.
+  if (!__next_is_in_handed_off_code()) {
+    std::fprintf(stderr, "%s", msg);
+  }
+
+  __builtin_trap();
 }
 
 }  // namespace Impl
